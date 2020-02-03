897e0cee0489bfd845cd058d0846a794ebdcb559
Andrei Popescu <andreipopescu@Andreis-MacBook-Pro.local>
2020-02-03 18:13:35 +0200

    Defined the structure of the application and add a coule of test cards; create a lading page top element

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import Card from './components/card/Card';
+import Landing from './components/landing/Landing';
+import './styles/App.css';
+
+/**
+ *  Test array for showing and testing the display properties
+ * 
+ */
+const cards = [
+  {
+      destination: 'Madrid',
+      cost: 300
+  },
+  {
+      destination: 'Lisabona',
+      cost: 200
+  },
+  {
+      destination: 'London',
+      cost: 400
+  },
+  {
+      destination: 'Roma',
+      cost: 125
+  }
+];
 
 const App = () => {
   return (
     <div className="app">
       <div className="landing-page">
-
+        <Landing/>
       </div>
-      <div className="additional-content">
-
+      <div className="content">
+        {cards.map(item => {
+            return <Card destination={item.destination} cost={item.cost}></Card>
+        })}
       </div>
     </div>
   );
---
+++ front-end/src/components/landing/Landing.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../styles/Lading.css';
+
+
+class Lading extends React.Component{
+    render() {
+        return (
+            <div className="landing">
+                <div className="brand">
+                    <h3 className="brand-name">
+                        The way to travel wherever you want
+                    </h3>
+                </div>
+            </div>
+        )
+    }
+}
+    
+export default Lading;
