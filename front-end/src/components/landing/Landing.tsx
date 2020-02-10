4aaac88c0a5251ffa8ea2bf860612981afe1ffb7
Andrei Popescu <andreipopescu@Andreis-MacBook-Pro.local>
2020-02-10 22:31:05 +0200

    Finish separation for the two menu types from lading page

--- front-end/src/components/landing/Landing.tsx
+++ front-end/src/components/landing/Landing.tsx
@@ -7,6 +7,8 @@
 import React from "react";
 import "../../styles/Lading.css";
 import { newsProcessor, Article} from './services/newsService';
+import SideMenu from '../menu/SideMenu';
+import TopMenu from '../menu/TopMenu';
 
 export enum Visibility {
   Visibile,
@@ -47,110 +49,11 @@ class Lading extends React.Component<{}, LandingState> {
     window.addEventListener("resize", this.updateDimensions);
   }
 
-  /**
-   * Side menu for landing page
-   */
-  sideMenu = () => {
-    return (
-      <nav
-        className="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding"
-        id="main-sidebar"
-      >
-        <br />
-        <div className="w3-container">
-          <h3 className="w3-padding-64">
-            <b>
-              <i className="fas fa-plane-departure"></i>GO
-              <br />
-              Travel
-            </b>
-          </h3>
-
-          {/* External links: TODO separate them from the menu in external function + align them */}
-          <div className="w3-container ">
-          <a href="#" className="w3-bar-item w3-button">
-              <i className="fas fa-search"></i>
-            </a>
-            <a href="#" className="w3-bar-item w3-button">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="#" className="w3-bar-item w3-button">
-              <i className="fab fa-twitter-square"></i>
-            </a>
-            <a href="#" className="w3-bar-item w3-button">
-              <i className="fab fa-linkedin"></i>
-            </a> 
-          </div>
-        </div>
-
-        {/*
-          The routing process for all the other entries of the program
-        */}
-
-        <div className="w3-bar-block">
-        
-          <button
-            onClick={() => console.log("hello")}
-            className="w3-bar-item w3-button w3-hover-white"
-          >
-            Home
-          </button>
-          <button
-            onClick={() => console.log("hello")}
-            className="w3-bar-item w3-button w3-hover-white"
-          >
-            About us
-          </button>
-          <button
-            onClick={() => console.log("hello")}
-            className="w3-bar-item w3-button w3-hover-white"
-          >
-            Services
-          </button>
-          <button
-            onClick={() => console.log("hello")}
-            className="w3-bar-item w3-button w3-hover-white"
-          >
-            Login <i className="fas fa-male"></i>
-          </button>
-          <button
-            onClick={() => console.log("hello")}
-            className="w3-bar-item w3-button w3-hover-white"
-          >
-            Contact
-          </button>
-          
-        </div>
-      </nav>
-    );
-  };
-
-  /**
-   * Responsive small screens menu icon
-   */
-  topMenuSmallScreens = () => {
-    return (
-      <header className="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
-        <button
-          className="w3-button w3-hover-white w3-margin-right"
-          onClick={() => console.log("hello")}
-        >
-          Login <i className="fas fa-male"></i>
-        </button>
-        <span>
-          <b>
-            <i className="fas fa-plane-departure"></i>GO Travel
-          </b>
-        </span>
-      </header>
-    );
-  };
-
   renderMenu = () => {
     if (this.state.sideMenuVisibility === Visibility.Visibile) {
-      return this.sideMenu();
+      return <SideMenu/>
     } else {
-      return this.topMenuSmallScreens();
+      return <TopMenu/>;
     }
   };
 
--- front-end/src/components/menu/SideMenu.tsx
+++ front-end/src/components/menu/SideMenu.tsx
@@ -1,7 +1,80 @@
 import React from 'react';
 
 class SideMenu extends React.Component<{}, {}> {
-   
+   render() {
+    return (
+        <nav
+          className="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding"
+          id="main-sidebar"
+        >
+          <br />
+          <div className="w3-container">
+            <h3 className="w3-padding-64">
+              <b>
+                <i className="fas fa-plane-departure"></i>GO
+                <br />
+                Travel
+              </b>
+            </h3>
+  
+            {/* External links: TODO separate them from the menu in external function + align them */}
+            <div className="w3-container ">
+            <a href="#" className="w3-bar-item w3-button">
+                <i className="fas fa-search"></i>
+              </a>
+              <a href="#" className="w3-bar-item w3-button">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="#" className="w3-bar-item w3-button">
+                <i className="fab fa-twitter-square"></i>
+              </a>
+              <a href="#" className="w3-bar-item w3-button">
+                <i className="fab fa-linkedin"></i>
+              </a> 
+            </div>
+          </div>
+  
+          {/*
+            The routing process for all the other entries of the program
+          */}
+  
+          <div className="w3-bar-block">
+          
+            <button
+              onClick={() => console.log("hello")}
+              className="w3-bar-item w3-button w3-hover-white"
+            >
+              Home
+            </button>
+            <button
+              onClick={() => console.log("hello")}
+              className="w3-bar-item w3-button w3-hover-white"
+            >
+              About us
+            </button>
+            <button
+              onClick={() => console.log("hello")}
+              className="w3-bar-item w3-button w3-hover-white"
+            >
+              Services
+            </button>
+            <button
+              onClick={() => console.log("hello")}
+              className="w3-bar-item w3-button w3-hover-white"
+            >
+              Login <i className="fas fa-male"></i>
+            </button>
+            <button
+              onClick={() => console.log("hello")}
+              className="w3-bar-item w3-button w3-hover-white"
+            >
+              Contact
+            </button>
+            
+          </div>
+        </nav>
+      );
+   }
 }
 
 export default SideMenu;
