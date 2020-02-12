3620b6a0749d19e477dd3b3477c4310cb529b2a8
Andrei Popescu <andrei.popescu@nxp.com>
2020-02-12 17:32:10 +0200

    Added a basic routing method

--- front-end/src/components/menu/SideMenu.tsx
+++ front-end/src/components/menu/SideMenu.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Lading from '../landing/Landing';
+import AboutUs from '../about-us/AboutUs';
+import Offert from '../offert/Offert';
 
 class SideMenu extends React.Component<{}, {}> {
    render() {
@@ -7,6 +16,7 @@ class SideMenu extends React.Component<{}, {}> {
           className="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding"
           id="main-sidebar"
         >
+          {/* App name */}
           <br />
           <div className="w3-container">
             <h3 className="w3-padding-64">
@@ -37,41 +47,59 @@ class SideMenu extends React.Component<{}, {}> {
           {/*
             The routing process for all the other entries of the program
           */}
-  
+        <Router>
           <div className="w3-bar-block">
           
             <button
               onClick={() => console.log("hello")}
               className="w3-bar-item w3-button w3-hover-white"
             >
-              Home
+             <Link to="/">Home</Link> 
             </button>
             <button
               onClick={() => console.log("hello")}
               className="w3-bar-item w3-button w3-hover-white"
             >
-              About us
+              <Link to="/about-us">About Us</Link>
             </button>
             <button
               onClick={() => console.log("hello")}
               className="w3-bar-item w3-button w3-hover-white"
             >
-              Services
+               <Link to="/services">Services</Link>
             </button>
             <button
               onClick={() => console.log("hello")}
               className="w3-bar-item w3-button w3-hover-white"
             >
-              Login <i className="fas fa-male"></i>
+              <Link to="/login">Login</Link> <i className="fas fa-male"></i>
             </button>
             <button
               onClick={() => console.log("hello")}
               className="w3-bar-item w3-button w3-hover-white"
             >
-              Contact
+              <Link to="/">Contact</Link>
             </button>
             
           </div>
+          <Switch>
+          <Route exact path="/">
+            <Lading />
+          </Route>
+          <Route path="/about-us">
+            <AboutUs />
+          </Route>
+          <Route path="/services">
+            < Offert/>
+          </Route>
+          <Route path="/login">
+            < Lading/>
+          </Route>
+          <Route path="/contact">
+            < Lading/>
+          </Route>
+        </Switch>
+          </Router>
         </nav>
       );
    }
