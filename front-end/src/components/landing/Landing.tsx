b25ec8a585a179c594695397c5a3ee9b671f17c6
Andrei Popescu <andreipopescu@Andreis-MacBook-Pro.local>
2020-02-12 23:47:47 +0200

    Reformat landing page for basic routing

--- front-end/src/components/landing/Landing.tsx
+++ front-end/src/components/landing/Landing.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "../../styles/Lading.css";
-import { newsProcessor, Article} from './services/newsService';
-import SideMenu from '../menu/SideMenu';
-import TopMenu from '../menu/TopMenu';
-import ArtcileComponent from '../article/Article';
+import { newsProcessor, Article } from "./services/newsService";
+import TopMenu from "../menu/TopMenu";
+import ArtcileComponent from "../article/Article";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export enum Visibility {
   Visibile,
@@ -31,8 +31,8 @@ class Lading extends React.Component<{}, LandingState> {
 
   processData = async () => {
     let data: Article[] = await newsProcessor();
-    this.setState({content: data});
-  }
+    this.setState({ content: data });
+  };
 
   componentDidMount() {
     this.updateDimensions();
@@ -40,16 +40,96 @@ class Lading extends React.Component<{}, LandingState> {
     window.addEventListener("resize", this.updateDimensions);
   }
 
+  socialMedia = () => {
+    return (
+      <div className="w3-container ">
+        <a href="/" className="w3-bar-item w3-button">
+          <i className="fas fa-search"></i>
+        </a>
+        <a href="/" className="w3-bar-item w3-button">
+          <i className="fab fa-instagram"></i>
+        </a>
+        <a href="/" className="w3-bar-item w3-button">
+          <i className="fab fa-twitter-square"></i>
+        </a>
+        <a href="/" className="w3-bar-item w3-button">
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </div>
+    );
+  };
+
+  menuEntries = () => {
+    return (
+      <Router>
+        <div className="w3-bar-block">
+          <button
+            onClick={() => console.log("hello")}
+            className="w3-bar-item w3-button w3-hover-white"
+          >
+            <Link to="/home">Home</Link>
+          </button>
+          <button
+            onClick={() => console.log("hello")}
+            className="w3-bar-item w3-button w3-hover-white"
+          >
+            <Link to="/about-us">About Us</Link>
+          </button>
+          <button
+            onClick={() => console.log("hello")}
+            className="w3-bar-item w3-button w3-hover-white"
+          >
+            <Link to="/services">Services</Link>
+          </button>
+          <button
+            onClick={() => console.log("hello")}
+            className="w3-bar-item w3-button w3-hover-white"
+          >
+            <Link to="/user">User</Link> <i className="fas fa-male"></i>
+          </button>
+          <button
+            onClick={() => console.log("hello")}
+            className="w3-bar-item w3-button w3-hover-white"
+          >
+            <Link to="/contact">Contact</Link>
+          </button>
+        </div>
+      </Router>
+    );
+  };
+
+  sideMenu = () => {
+    return (
+      <nav
+        className="w3-sidebar w3-red w3-collapse w3-top w3-large w3-padding"
+        id="main-sidebar"
+      >
+        <br />
+        <div className="w3-container">
+          <h3 className="w3-padding-64">
+            <b>
+              <i className="fas fa-plane-departure"></i>GO
+              <br />
+              Travel
+            </b>
+          </h3>
+          {this.socialMedia()}
+        </div>
+        {this.menuEntries()}
+      </nav>
+    );
+  };
+
   renderMenu = () => {
     if (this.state.sideMenuVisibility === Visibility.Visibile) {
-      return <SideMenu/>
+      return this.sideMenu();
     } else {
-      return <TopMenu/>;
+      return <TopMenu />;
     }
   };
 
   generateArticleFormat = (article: Article) => {
-    return <ArtcileComponent artcile={article}/>
+    return <ArtcileComponent artcile={article} />;
   };
 
   renderPageContent = () => {
@@ -67,11 +147,11 @@ class Lading extends React.Component<{}, LandingState> {
           <hr />
         </div>
         <div className="w3-container">
-          <p className="w3-large w3-text-red">
-            Latest news on Travel topic:
-          </p>
-          <hr/>
-          {this.state.content.map(article => this.generateArticleFormat(article))}
+          <p className="w3-large w3-text-red">Latest news on Travel topic:</p>
+          <hr />
+          {this.state.content.map(article =>
+            this.generateArticleFormat(article)
+          )}
         </div>
       </div>
     );
